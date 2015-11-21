47181f545eb0222059107d47bede27964df1087f
Vasilena Dragancheva <vasilena86@hotmail.com>
2015-11-21 21:30:57 +0200

    Added Bit Array

---
+++ StaticMembersAndNamespaces/Problem6/BitArray.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace Problem6
+{
+    class BitArray
+    {
+        private const int MAX_LENGTH = 100000;
+        private const int MIN_LENGTH = 1;
+        private int length;
+        private int[] bits;
+
+        public BitArray(int length)
+        {
+            this.Length = length;
+
+        }
+        public int Length
+        {
+            get
+            {
+                return this.length;
+            }
+            set
+            {
+                if (value > MAX_LENGTH||value<MIN_LENGTH)
+                {
+                    throw new ArgumentOutOfRangeException("Max length is in range of 1 to 100 000 elements.");
+                }
+                this.length = value;
+                this.bits = new int[this.Length];
+            }
+            
+        }
+
+        private BigInteger GetDecimalValue()
+        {
+            BigInteger result = 0;
+            for (int i = 0; i < this.bits.Length; i++)
+            {
+                int currBit = this.bits[i];
+                BigInteger n=0;
+                if(currBit==1)
+                {
+                    n=1;
+                    for (int j = 0; j < i; j++)
+                    {
+                        n *= 2;
+                    }
+                }
+                result += n;
+            }
+            return result;
+
+        }
+        
+        public int this[int index]
+        {
+            get
+            {
+                if(index>=this.Length||index<0)
+                {
+                    throw new IndexOutOfRangeException("Index is outside boundries of BitArray");
+                }
+                return this.bits[index];
+            }
+            set
+            {
+                if(index>=this.Length||index<0)
+                {
+                    throw new IndexOutOfRangeException("Index is outside boundries of BitArray");
+                }
+                if (value != 0 && value != 1)
+                {
+                    throw new ArgumentException("Bit value can be only 0 or 1");
+                }
+                this.bits[index] = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            return this.GetDecimalValue().ToString();
+        }
+       
+    }
+}
--- StaticMembersAndNamespaces/Problem6/TestingBitArray.cs
+++ StaticMembersAndNamespaces/Problem6/TestingBitArray.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Numerics;
 
 namespace Problem6
 {
@@ -10,6 +11,13 @@ namespace Problem6
     {
         static void Main(string[] args)
         {
+            BitArray bit = new BitArray(100000);
+            bit[0] = 1;
+            bit[12] = 1;
+            bit[100000 - 1] = 1;
+            Console.WriteLine(bit);
+            
+           
         }
     }
 }
